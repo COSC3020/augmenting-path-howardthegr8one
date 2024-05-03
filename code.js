2c61bfb6cf2e06d27a72da8cec986b53e89a22b4
Howard Shaw <68131536+howardthegr8one@users.noreply.github.com>
2024-05-03 12:10:52 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,34 @@
+// DISCLAIMER: I worked with Isaiah Elston on this exercise, we pair programmed the augmenting path
+// function as well as came up with some extra tests and did the runtime together. 
+
 function augmentingPath(graph, start, end) {
-    return [];
+  if (start == end) {
+    return [start]
+  }
+
+  let queue = [[start]]
+  let visited = []
+
+  let nodes = Object.keys(graph)
+  if (nodes.length == 1) return nodes
+
+  while (queue.length > 0) {
+      let path = queue.shift()
+      let node = path[path.length - 1]
+
+      if (node == end) {
+        return path
+      }
+
+      if (!visited.includes(node)) {
+          visited.push(node)
+
+          let neighbors = Object.keys(graph[node] || {})
+          for (let neighbor of neighbors) {
+              let newPath = [...path, neighbor]
+              queue.push(newPath)
+          }
+      }
+  }
+  return [];
 }
